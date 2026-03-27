61e97d3901e89398103171ec16b3addd1c833661
Lazer Sharks in Space <hsrobotics@teameureka.net>
2026-03-27 08:50:17 -0600

    New opmode!
    This is an opmode to find Distance in inches using the Limelight 3a.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightDistance.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class LimelightDistance extends OpMode {
+
+    private Limelight3A limelight3A;
+
+    private double Camera_Height_In = 10.96;
+    private double Camera_Angle = 20;
+    private double Goal_Height = 29.5;
+    private double distance = 0;
+    @Override
+    public void init() {
+        limelight3A = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight3A.pipelineSwitch(8);
+        limelight3A.start();
+    }
+
+
+    @Override
+    public void loop() {
+        LLResult llResult = limelight3A.getLatestResult();
+
+        if (llResult != null && llResult.isValid()) {
+            telemetry.addData("Distance", distance);
+        } else {
+            telemetry.addData("No Target", "Found");
+        }
+
+    }
+
+    public double getDistance(double ty) {
+        double angleToTarget = Camera_Angle + ty;
+        double heightDifference = Goal_Height - Camera_Height_In;
+        return heightDifference / Math.tan(Math.toRadians(angleToTarget));
+    }
+}
