3f7661fbba51bb25fb33ef2a7bd8ad9ef87a9ee4
Lazer Sharks in Space <hsrobotics@teameureka.net>
2026-05-01 11:37:58 -0600

    Fresh Reinstallation
    Done to fix problems with Pedro Pathing. Installed QuickStart, put opmodes back, and starting to retune.
    
    Co-Authored-By: Team 8271 <ehackers8271@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,8 +1,5 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
-import com.pedropathing.control.FilteredPIDFCoefficients;
-import com.pedropathing.control.PIDFCoefficients;
-import com.pedropathing.control.PredictiveBrakingCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -17,32 +14,19 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-    // Put Robot weight in kg & 10.2 kg in pounds is 22.5 pounds
-            .mass(13.1) // was 10.22
-            .centripetalScaling(0.001)
-            .predictiveBrakingCoefficients(new PredictiveBrakingCoefficients(.05, .095, .0021693))
-            .translationalPIDFCoefficients(new PIDFCoefficients(1.5, 0, 0.01, 0.01))
-            .headingPIDFCoefficients(new PIDFCoefficients(1.2, 0, 0, 0))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(.15,0,0.004,0.5,.03))
+    .mass(13.1);
 
-            .forwardZeroPowerAcceleration(-27.577491485309178)
-            .lateralZeroPowerAcceleration(-60.75657833514421);
-
-
-
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, .75, .3);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
-                .pinpointLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
                 .build();
     }
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
-            .xVelocity(67) // was 72
-            .yVelocity(49) // was 55
             .rightFrontMotorName("fr")
             .rightRearMotorName("br")
             .leftRearMotorName("bl")
@@ -53,13 +37,11 @@ public class Constants {
             .rightRearMotorDirection(DcMotorEx.Direction.FORWARD);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(-4.4)
-            // -4.4
-            // 2.7
-            .strafePodX(2.7)
+            .forwardPodY(1.73)
+            .strafePodX(-6.3345)
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 }
